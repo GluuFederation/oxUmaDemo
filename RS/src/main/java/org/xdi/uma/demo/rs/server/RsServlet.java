77b1b2d688a2e072f19a5a09d97312bcc342c177
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2016-04-29 19:11:32 +0300

    removed old uma configs

--- RS/src/main/java/org/xdi/uma/demo/rs/server/RsServlet.java
+++ RS/src/main/java/org/xdi/uma/demo/rs/server/RsServlet.java
@@ -3,7 +3,6 @@ package org.xdi.uma.demo.rs.server;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 import org.apache.log4j.Logger;
 import org.jboss.resteasy.spi.ResteasyProviderFactory;
-import org.xdi.oxauth.model.uma.UmaConfiguration;
 import org.xdi.oxd.rs.protect.RsProtector;
 import org.xdi.oxd.rs.protect.RsResource;
 import org.xdi.oxd.rs.protect.resteasy.ConfigurationLoader;
@@ -13,10 +12,7 @@ import org.xdi.oxd.rs.protect.resteasy.ServiceProvider;
 import org.xdi.uma.demo.common.gwt.Msg;
 import org.xdi.uma.demo.common.server.CommonUtils;
 import org.xdi.uma.demo.common.server.Configuration;
-import org.xdi.uma.demo.common.server.Uma;
-import org.xdi.uma.demo.common.server.ref.IMetadataConfiguration;
 import org.xdi.uma.demo.rs.client.Service;
-import org.xdi.util.InterfaceRegistry;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -39,29 +35,21 @@ public class RsServlet extends RemoteServiceServlet implements Service {
         try {
             final Configuration c = Configuration.getInstance();
             if (c != null) {
-                final UmaConfiguration umaAmConfiguration = Uma.discovery(c.getUmaMetaDataUrl());
-                if (umaAmConfiguration != null) {
-                    InterfaceRegistry.put(IMetadataConfiguration.class, umaAmConfiguration);
-                    LOG.info("Loaded UMA configuration: " + CommonUtils.asJsonSilently(umaAmConfiguration));
+                ClassLoader classLoader = ConfigurationLoader.class.getClassLoader();
+                org.xdi.oxd.rs.protect.resteasy.Configuration configuration = ConfigurationLoader.loadFromJson(classLoader.getResourceAsStream("rs-protect-config.json"));
 
-                    ClassLoader classLoader = ConfigurationLoader.class.getClassLoader();
-                    org.xdi.oxd.rs.protect.resteasy.Configuration configuration = ConfigurationLoader.loadFromJson(classLoader.getResourceAsStream("rs-protect-config.json"));
-                    Collection<RsResource> values = RsProtector.instance(classLoader.getResourceAsStream("rs-protect.json")).getResourceMap().values();
+                Collection<RsResource> values = RsProtector.instance(classLoader.getResourceAsStream("rs-protect.json")).getResourceMap().values();
 
-                    ServiceProvider serviceProvider = new ServiceProvider(configuration);
-                    PatProvider patProvider = new PatProvider(serviceProvider);
-                    ResourceRegistrar resourceRegistrar = new ResourceRegistrar(patProvider);
+                ServiceProvider serviceProvider = new ServiceProvider(configuration);
+                PatProvider patProvider = new PatProvider(serviceProvider);
+                ResourceRegistrar resourceRegistrar = new ResourceRegistrar(patProvider);
 
-                    resourceRegistrar.register(values);
+                resourceRegistrar.register(values);
 
-                    ResteasyProviderFactory.pushContext(PatProvider.class, patProvider);
-                    ResteasyProviderFactory.pushContext(ResourceRegistrar.class, resourceRegistrar);
+                ResteasyProviderFactory.pushContext(PatProvider.class, patProvider);
+                ResteasyProviderFactory.pushContext(ResourceRegistrar.class, resourceRegistrar);
 
-                    LOG.info("Resource Server started successfully.");
-                } else {
-                    LOG.error("Unable to load Authorization Server configuration. Failed to start Resource Server.");
-                }
-
+                LOG.info("Resource Server started successfully.");
             }
         } catch (Exception e) {
             LOG.error(e.getMessage(), e);
