89f5632f08ad408f6ebc967131b24eab25ea4084
yuriyz <yzabrovarniy@gmail.com>
2016-05-04 17:21:51 +0300

    RS : added configuration loading from <tomcat home>/conf/<configs>

--- RS/src/main/java/org/xdi/uma/demo/rs/server/RsServlet.java
+++ RS/src/main/java/org/xdi/uma/demo/rs/server/RsServlet.java
@@ -6,10 +6,7 @@ import org.apache.log4j.Logger;
 import org.jboss.resteasy.spi.ResteasyProviderFactory;
 import org.xdi.oxd.rs.protect.RsProtector;
 import org.xdi.oxd.rs.protect.RsResource;
-import org.xdi.oxd.rs.protect.resteasy.ConfigurationLoader;
-import org.xdi.oxd.rs.protect.resteasy.PatProvider;
-import org.xdi.oxd.rs.protect.resteasy.ResourceRegistrar;
-import org.xdi.oxd.rs.protect.resteasy.ServiceProvider;
+import org.xdi.oxd.rs.protect.resteasy.*;
 import org.xdi.uma.demo.common.gwt.Msg;
 import org.xdi.uma.demo.common.server.LogList;
 import org.xdi.uma.demo.common.server.ref.ILogList;
@@ -18,6 +15,10 @@ import org.xdi.util.InterfaceRegistry;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.Collection;
 import java.util.List;
 
@@ -30,18 +31,35 @@ public class RsServlet extends RemoteServiceServlet implements Service {
 
     private static final Logger LOG = Logger.getLogger(RsServlet.class);
 
+    static {
+        if ((System.getProperty("catalina.base") != null) && (System.getProperty("catalina.base.ignore") == null)) {
+            BASE_DIR = System.getProperty("catalina.base");
+        } else if (System.getProperty("catalina.home") != null) {
+            BASE_DIR = System.getProperty("catalina.home");
+        } else if (System.getProperty("jboss.home.dir") != null) {
+            BASE_DIR = System.getProperty("jboss.home.dir");
+        } else {
+            BASE_DIR = null;
+        }
+    }
+
+    private static final String BASE_DIR;
+    private static final String DIR = BASE_DIR + File.separator + "conf" + File.separator;
+
+    public static final String CONFIGURATION_FILE_NAME = "rs-protect-config.json";
+    public static final String PROTECTION_CONFIGURATION_FILE_NAME = "rs-protect.json";
+
     @Override
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
 
         try {
-            ClassLoader classLoader = ConfigurationLoader.class.getClassLoader();
-            org.xdi.oxd.rs.protect.resteasy.Configuration configuration = ConfigurationLoader.loadFromJson(classLoader.getResourceAsStream("rs-protect-config.json"));
+            Configuration configuration = ConfigurationLoader.loadFromJson(inputStream(CONFIGURATION_FILE_NAME));
             LOG.info("Loaded configuration: " + configuration);
 
-            Collection<RsResource> values = RsProtector.instance(classLoader.getResourceAsStream("rs-protect.json")).getResourceMap().values();
+            Collection<RsResource> values = RsProtector.instance(inputStream(PROTECTION_CONFIGURATION_FILE_NAME)).getResourceMap().values();
 
-            LOG.info("Protection configuration: " + IOUtils.toString(classLoader.getResourceAsStream("rs-protect.json")));
+            LOG.info("Protection configuration: " + IOUtils.toString(inputStream(PROTECTION_CONFIGURATION_FILE_NAME)));
 
             ServiceProvider serviceProvider = new ServiceProvider(configuration);
             PatProvider patProvider = new PatProvider(serviceProvider);
@@ -58,6 +76,16 @@ public class RsServlet extends RemoteServiceServlet implements Service {
             LOG.error("Failed to start Resource Server. " +e.getMessage(), e);
             throw new ServletException(e);
         }
+    }
+
+    private InputStream inputStream(String fileName) throws FileNotFoundException {
+        ClassLoader classLoader = ConfigurationLoader.class.getClassLoader();
+        File file = new File(DIR + fileName);
+        if (file.exists()) {
+            LOG.trace("Configuration file location: " + DIR + fileName);
+            return new FileInputStream(file);
+        }
+        return classLoader.getResourceAsStream(fileName);
     }
 
     @Override
